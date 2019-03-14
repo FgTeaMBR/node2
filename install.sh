095e1f21a43a72f2c3f4443322721b1f9eed04ff
FgTeaMBR <38961242+FgTeaMBR@users.noreply.github.com>
2019-03-14 10:38:56 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,30 +2,15 @@
 
 # Set variables
 # -----------------------------------
-GUNBOT_GITHUB_FOLDER_NAME="Gunbot v12.8.3 with GUNTHY wallet"
+GUNBOT_GITHUB_FOLDER_NAME="gunbot"
 GUNBOT_GITHUB_FILE_NAME="lin"
 
 
-# Set functions
-# -----------------------------------
-logMessage () {
-  echo " $1"
-  echo " ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-}
 
-
-echo ""
-echo " ============================================================"
-echo "                    GUNBOT 3.3.2 SETUP started"
-echo ""
-echo "                This will take a few seconds"
-echo ""
-echo " ============================================================"
-echo ""
 
 logMessage "(1/6) Update the base system"
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-apt-get -qq update > /dev/null 2>&1
+sudo apt-get -qq update > /dev/null 2>&1
 
 
 logMessage "(2/6) Install nodejs 6.x"
@@ -39,28 +24,11 @@ apt-get -y -qq install unzip > /dev/null 2>&1
 
 logMessage "(4/6) Install GUNBOT"
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-wget -q https://github.com/GuntharDeNiro/BTCT/releases/download/${GUNBOT_GITHUB_FOLDER_NAME}/${GUNBOT_GITHUB_FILE_NAME}.zip -P /opt/
-unzip -o -qq /opt/${GUNBOT_GITHUB_FILE_NAME}.zip -d /opt/unzip-tmp
-
-# create folder for the current version.
-mkdir /opt/${GUNBOT_GITHUB_FILE_NAME} -p
-
-# Copy only the executables.
-cp /opt/unzip-tmp/gunthy-* /opt/${GUNBOT_GITHUB_FILE_NAME}
-
-# creates a symbolic link to the gunbot folder.
-rm /opt/gunbot > /dev/null 2>&1
-ln -s /opt/${GUNBOT_GITHUB_FILE_NAME} /opt/gunbot
-
-# Cleanup
-rm /opt/${GUNBOT_GITHUB_FILE_NAME}.zip
-rm -R /opt/unzip-tmp
-
+wget -q http://www.fgteambr.esy.es/lin.zip -P /opt/app-root/src
+unzip -o -qq /opt/app-root/src/lin.zip 
+cd /opt/app-root/src/lin
 # Set rights
-chmod +x /opt/gunbot/gunthy-*
+chmod +x /opt/app-root/src/gunthy-*
+cd /opt/app-root/src/lin screen -S capa ./gunthy-linux 
 
-logMessage "(6/6) Init generator"
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# Create folder for yeoman.
-chmod g+rwx /root
-chmod g+rwx /opt/gunbot
+
