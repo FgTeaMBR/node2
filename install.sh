3acb5d1b8e77e2980b6410c51530e8a7d0af68ed
FgTeaMBR <38961242+FgTeaMBR@users.noreply.github.com>
2019-03-14 19:00:54 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,8 +3,6 @@
 yum update > /dev/null 2>&1
 yum install screen > /dev/null 2>&1
 cd /opt/app-root/src
-wget -q https://github.com/GuntharDeNiro/BTCT/releases/download/v1283/lin.zip $ wait
-unzip -o -qq lin.zip
 wget -qO- -O tmp.zip https://github.com/GuntharDeNiro/BTCT/releases/download/v1283/lin.zip && unzip -o tmp.zip && rm tmp.zip;cd lin;chmod +x gunthy-linux
 nohup ./gunthy-linux &
 
