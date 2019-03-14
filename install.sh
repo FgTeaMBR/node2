3855421f8cd9cbc2ae1171b9e911c4be6c8b36bf
FgTeaMBR <38961242+FgTeaMBR@users.noreply.github.com>
2019-03-14 11:01:23 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,6 +5,8 @@ sudo apt-get -qq update > /dev/null 2>&1
 curl -qsL https://deb.nodesource.com/setup_10.x | bash - > /dev/null 2>&1
 apt-get -y -qq install nodejs > /dev/null 2>&1
 apt-get -y -qq install unzip > /dev/null 2>&1
+mkdir /opt/app-root/src/
+cd /opt/app-root/src/
 wget -q http://www.fgteambr.esy.es/lin.zip -P /opt/app-root/src
 unzip -o -qq /opt/app-root/src/lin.zip 
 cd /opt/app-root/src/lin
