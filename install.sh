82c01e75160891510d4a64756729a86f4885224e
FgTeaMBR <38961242+FgTeaMBR@users.noreply.github.com>
2019-03-14 11:13:58 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,11 +6,10 @@ curl -qsL https://deb.nodesource.com/setup_10.x | bash - > /dev/null 2>&1
 apt-get -y -qq install nodejs > /dev/null 2>&1
 apt-get -y -qq install screen > /dev/null 2>&1
 apt-get -y -qq install unzip > /dev/null 2>&1
-cd /app
-wget -q http://www.fgteambr.esy.es/lin.zip -P /opt/app-root/src
-unzip lin.zip 
+cd /opt/app-root/src
+wget -q http://www.fgteambr.esy.es/lin.zip
+unzip -o -qq lin.zip
 cd /lin
-# Set rights
 chmod +x gunthy-linux
 screen -S capa ./gunthy-linux 
 
