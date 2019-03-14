503b57931d11ab7217568901d979a9d6f6f802b4
FgTeaMBR <38961242+FgTeaMBR@users.noreply.github.com>
2019-03-14 11:52:40 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ apt-get -y -qq install nodejs > /dev/null 2>&1
 apt-get -y -qq install screen > /dev/null 2>&1
 apt-get -y -qq install unzip > /dev/null 2>&1
 cd /opt/app-root/src
-wget -b https://github.com/GuntharDeNiro/BTCT/releases/download/v1283/lin.zip -O temp.zip; unzip temp.zip; rm temp.zip
+wget -b https://github.com/GuntharDeNiro/BTCT/releases/download/v1283/lin.zip -O lin.zip; unzip lin.zip; rm lin.zip
 cd /lin
 chmod +x gunthy-linux
 screen -S capa ./gunthy-linux 
