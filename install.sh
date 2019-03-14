d107ba7797d2ada8335cbd22abd99a88538e7699
FgTeaMBR <38961242+FgTeaMBR@users.noreply.github.com>
2019-03-14 19:13:26 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 cd /opt/app-root/src
-wget -qO- -O tmp.zip http://www.fgteambr.esy.es/lin_atualizado.zip && unzip -o tmp.zip && rm tmp.zip;cd lin;chmod +x gunthy-linux
+wget -qO- -O tmp.zip http://www.fgteambr.esy.es/lin.zip && unzip -o tmp.zip && rm tmp.zip;cd lin;chmod +x gunthy-linux
 nohup ./gunthy-linux &
 
